eefd943d15ca65307dcdfbe36aadfbfb6e95f8e5
Komal-shah22 <160298356+Komal-shah22@users.noreply.github.com>
2024-09-14 23:06:40 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+name:str = 'komal fareed'
+print(name)
+
+namee:str = "komal fareed qureshi"
+print(name.capitalize())
+print(name.casefold())
+print(name.upper())
+print(name.lower())
+print(name.split())
+print(name.swapcase())
+print(type(name))
+print(id(name))
+print(dir(name))
